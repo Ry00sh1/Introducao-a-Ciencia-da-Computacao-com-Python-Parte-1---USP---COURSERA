341e51651e8c191722cf8bd9a6240e312253e6ff
Pedro Sena <phasena@gmail.com>
2023-02-07 23:03:27 -0300

    Alterações
    - Renomeado o exercício de Bhaskara
    - Atualização do código do jogo do NIM

--- Exercicio_JogoDoNIM.py
+++ Exercicio_JogoDoNIM.py
@@ -1,6 +1,6 @@
-print("Bem-vindo ao jogo do NIM")
+print("Bem-vindo ao jogo do NIM! Escolha:")
 
-def principal():
+def inicio():
     print("1 - para jogar uma partida isolada\n2 - para jogar um campeonato\n")
 
     x = int(input())
@@ -15,27 +15,40 @@ def principal():
         else:
             print("Digite uma opção válida\n")
             x = 0
-            return principal()
+            return inicio()
 
-def pecas_Iniciais(n):
-    n = int(input("Digite a quantidade de peças iniciais: "))
-    return
+def computador_escolhe_jogada(n,m):
 
-def max_Rodada(m):
-    m = int(input("Máximo de peças que é possível retirar nessa rodada: "))
-    return
+def usuario_escolhe_jogada(n,m):
+
+def partida(n,m):
+    
+   
+    if n%(m+1) == 0:
+        print("Você começa!")
+        while n!= 0:
+            print(usuario_escolhe_jogada())
+    else:
+        print("Computador começa!")
+        while n!= 0:
+            print(computador_escolhe_jogada())
 
 def partida_Isolada(): 
-    x = 1; n = 0; m = 0
-    print("**** Rodada %d ****"%(x))
-    pecas_Iniciais(n);print(pecas_Iniciais(n))    
-    max_Rodada(m);print(max_Rodada(m))
+    print("Voce escolheu uma partida isolada!")
+    n = int(input("Quantas peças?"))
+    m = int(input("Limite de peças por jogada?"))
+    print(partida())
 
 def campeonato():
-    x = 1
-    print("**** Rodada %d ****"%(x))
+    print("Voce escolheu um campeonato!")
+
+    for x in range(4):
+        n = int(input("Quantas peças?"))
+        m = int(input("Limite de peças por jogada?"))
+        print("**** Rodada %d ****"%x)
+        print(partida())
     
-if principal()==1:
+if inicio()==1:
     print(partida_Isolada())
 else:
     print(campeonato())
