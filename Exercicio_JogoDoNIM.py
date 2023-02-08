c87530091ad76511bce99d7ce70ee56a34fe1679
Pedro Sena <phasena@gmail.com>
2023-02-08 20:36:00 -0300

    Finalização do exercício jogo do NIM
    Finalizei o programa-exercicio Jogo do NIM. Tive um pouco de dificuldade para entender oque foi solicitado e para desenvolver o código, porém depois de tanto estudo eu consegui.

--- Exercicio_JogoDoNIM.py
+++ Exercicio_JogoDoNIM.py
@@ -5,7 +5,7 @@ def inicio():
 
     x = int(input())
     
-    if (x!=1) or (x!=2):
+    if (x!=1) and (x!=2):
         if x == 1:
             print("Voce escolheu uma partida isolada!\n")
             return 1
@@ -17,36 +17,60 @@ def inicio():
             x = 0
             return inicio()
 
-def computador_escolhe_jogada(n,m):
-
-def usuario_escolhe_jogada(n,m):
-
-def partida(n,m):
+def computador_escolhe_jogada(n, m):
+    jogada = n % (m+1)
+    if jogada == 0:
+        jogada = m
+    return jogada
     
-   
-    if n%(m+1) == 0:
-        print("Você começa!")
-        while n!= 0:
-            print(usuario_escolhe_jogada())
+def usuario_escolhe_jogada(n, m):
+    jogada = int(input("Quantas peças você vai tirar? "))
+    while jogada > m or jogada > n or jogada < 1:
+        print("Limite máximo de peças igual a %d" % m)
+
+
+        jogada = int(input("Quantas peças você vai tirar? "))
+    return jogada
+
+def partida(n,m):   
+    if n % (m+1) == 0:
+        print("\nVocê começa!\n")
+        jogador = "usuario"
     else:
-        print("Computador começa!")
-        while n!= 0:
-            print(computador_escolhe_jogada())
+        print("\nComputador começa!\n")
+        jogador = "computador"
+    while n > 0:
+        if jogador == "usuario":
+            jogada = usuario_escolhe_jogada(n, m)
+            n = n - jogada
+            print("Você tirou", jogada, "peças.")
+            jogador = "computador"
+        else:
+            jogada = computador_escolhe_jogada(n, m)
+            n = n - jogada
+            print("O computador tirou", jogada, "peças.")
+            jogador = "usuario"
+        print("Agora restam", n, "peças no tabuleiro.\n")
+    if jogador == "usuario":
+        print("O computador ganhou!")
+    else:
+        print("Você ganhou!")
+
 
 def partida_Isolada(): 
     print("Voce escolheu uma partida isolada!")
     n = int(input("Quantas peças?"))
     m = int(input("Limite de peças por jogada?"))
-    print(partida())
+    print(partida(n,m))
 
 def campeonato():
     print("Voce escolheu um campeonato!")
 
-    for x in range(4):
+    for x in range(1,4):
         n = int(input("Quantas peças?"))
         m = int(input("Limite de peças por jogada?"))
         print("**** Rodada %d ****"%x)
-        print(partida())
+        print(partida(n,m))
     
 if inicio()==1:
     print(partida_Isolada())
